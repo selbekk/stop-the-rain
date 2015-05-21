6fb4611974ff0b775433a258044cd39ca21d7cd6
selbekk <kristofer.selbekk@bekk.no>
2015-05-21 12:52:29 +0200

    add some stuff that we need probably

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,11 +1,13 @@
-var express = require('express');
+var express = require('express'),
+    bodyParser = require('body-parser');
 
 var app = express();
 
 app.use('assets', express.static('webapp/public'));
+app.use(bodyParser.json());
 
 app.get('/api/check-weather', function(req, res, next) {
-    res.json({ isItRaining: true; });
+    res.json({ raining: true });
 });
 
 app.get('/api/order-sun', function(req, res, next) {
