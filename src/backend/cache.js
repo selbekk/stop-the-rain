861b2eb062078aaf4c99f55226ea37b55acbf9a7
selbekk <kristofer.selbekk@bekk.no>
2015-05-22 13:10:35 +0200

    Make the cache work correctly

--- src/backend/cache.js
+++ src/backend/cache.js
@@ -2,30 +2,28 @@ var moment = require('moment');
 
 var store = {};
 
-function Cache(opts) {
-    this.timeout = opts.timeout || 10; // minutes
-    this.timeoutUnit = opts.timeoutUnit || 'minutes';
-}
+var timeout = 10;
+var timeoutUnit = 'minutes';
+
 
 var makeKey = function(position) {
     return parseFloat(position.lat).toFixed(2) + ',' + parseFloat(position.lng).toFixed(2);
 };
 
-Cache.prototype.get = function(key) {
-    var hit = this.store[makeKey(key)];
+exports.get = function(key) {
+    var hit = store[makeKey(key)];
 
     if(!hit) return;
 
-    var isValid = moment().subtract(this.timeout, this.timeoutUnit).isAfter(hit.timestamp);
+    var isValid = moment().subtract(timeout, timeoutUnit).isBefore(hit.timestamp);
+
     return isValid ? hit.data : undefined;
 };
 
-Cache.prototype.put = function(key, data) {
-
-    this.store[makeKey(key)] = {
+exports.put = function(key, data) {
+    console.log('saving data as ', makeKey(key));
+    store[makeKey(key)] = {
         data: data,
         timestamp: moment()
     };
 };
-
-module.exports = Cache;
--- src/backend/weather-dao.js
+++ src/backend/weather-dao.js
@@ -1,6 +1,6 @@
 var request = require('request');
 
-var cache = require('./cache')();
+var cache = require('./cache');
 
 var url = 'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&APPID=ec7597638cf8f4cfb067e2ed0dc7f2d9';
 
@@ -12,7 +12,7 @@ exports.getWeather = function(position, callback) {
         console.log('returning a cached version of the weather');
         return callback(null, cached);
     }
-
+    console.log('not cached, hitting up the API');
     request(reqUrl, function(err, res, body) {
         if(err || res.statusCode !== 200) {
             console.error('problem fetching data from API', err);
