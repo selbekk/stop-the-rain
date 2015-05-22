09a1750ba4d9cd9c9dc5979f39b5b5028bc097ab
selbekk <kristofer.selbekk@bekk.no>
2015-05-22 10:57:51 +0200

    add a cache because reasons

--- src/backend/weather-dao.js
+++ src/backend/weather-dao.js
@@ -2,14 +2,22 @@ var request = require('request');
 
 var url = 'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&APPID=ec7597638cf8f4cfb067e2ed0dc7f2d9';
 
+var cache = {};
+
 exports.getWeather = function(position, callback) {
     var reqUrl = url.replace('{lat}', position.lat).replace('{lon}', position.lng);
+    if(cache[reqUrl]) {
+        return callback(null, cache[reqUrl]);
+    }
+
     request(reqUrl, function(err, res, body) {
         if(err || res.statusCode !== 200) {
             console.error('problem fetching data from API', err);
             return callback(err);
         }
-
-        callback(null, JSON.parse(body));
+        var result = JSON.parse(body);
+        cache[reqUrl] = result;
+        
+        callback(null, result);
     });
 };
