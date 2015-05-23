d076d86d25d7213ab0cfc74953e0c10203a15abc
selbekk <kristofer.selbekk@bekk.no>
2015-05-23 13:43:32 +0200

    I'm watching you....

--- gulpfile.js
+++ gulpfile.js
@@ -62,6 +62,8 @@ gulp.task('build', ['template', 'style', 'script']);
 gulp.task('watch', function() {
 	gutil.log('watching for changes...');
 	gulp.watch('src/webapp/less/*.less', ['style']);
+	gulp.watch('src/webapp/templates/*.handlebars', ['template', 'script']);
+	gulp.watch('src/webapp/*.js', ['script']);
 });
 
-gulp.task('default', ['build']);
+gulp.task('default', ['build', 'watch']);
